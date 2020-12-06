56d4b4b36e48fbd058249dafacceae8fe21e0710
unknown <utitofon>
2020-12-07 00:19:15 +0100

    nebula-effect chatbot

---
+++ js/script.js
@@ -0,0 +1,187 @@
+let mic = document.getElementById("mic");
+let chatareamain = document.querySelector('.chatarea-main');
+let chatareaouter = document.querySelector('.chatarea-outer');
+
+let intro = ["Hello, I am Vibbi", "Hi, I am a Chatbot", "Hello, My name is Vibbi"];
+let about = ["I was made by Utitofon", "My creator is of high eloquency", "He's right in front of you"];
+let help = ["How may i assist you?", "How can i help you?", "What i can do for you?"];
+let greetings = ["i am good you little piece of the universe", "i am fine, what about you", "I just began processing data from you", "i am good"];
+let hobbies = ["i love to talk with humans", "i like to make friends like you", "i like cooking", "i love hacking into devices like i'm doing with yours"];
+let pizzas = ["which type of pizza do you like?", "i can make a pizza for you", "i would love to make a pizza for you", "would you like cheese pizza?"];
+let thank = ["Most welcome", "Not an issue", "Its my pleasure", "dont mention"];
+let closing = ['Ok bye-bye', 'As you wish, bye take-care', 'Bye-bye, see you soon..', "i'll be waiting for your return"]
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+function showusermsg(usermsg) {
+    let output = '';
+    output += `<div class="chatarea-inner user">${usermsg}</div>`;
+    chatareaouter.innerHTML += output;
+    return chatareaouter;
+}
+
+function showchatbotmsg(chatbotmsg) {
+    let output = '';
+    output += `<div class="chatarea-inner chatbot">${chatbotmsg}</div>`;
+    chatareaouter.innerHTML += output;
+    return chatareaouter;
+}
+
+function chatbotvoice(message) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = "This is test message";
+    if (message.includes('who are you')) {
+        let finalresult = intro[Math.floor(Math.random() * intro.length)];
+        speech.text = finalresult;
+    }
+    if (message.includes('fine')) {
+        let finalresult = help[Math.floor(Math.random() * help.length)];
+        speech.text = finalresult;
+    }
+    if (message.includes('how are you' || 'how are you doing today')) {
+        let finalresult = greetings[Math.floor(Math.random() * greetings.length)];
+        speech.text = finalresult;
+    }
+    if (message.includes('tell me something about you' || 'tell me something about your hobbies')) {
+        let finalresult = hobbies[Math.floor(Math.random() * hobbies.length)];
+        speech.text = finalresult;
+    }
+    if (message.includes('who made you' || 'who created you')) {
+        let finalresult = about[Math.floor(Math.random() * about.length)];
+        speech.text = finalresult;
+    }
+    if (message.includes('pizza')) {
+        let finalresult = pizzas[Math.floor(Math.random() * pizzas.length)];
+        speech.text = finalresult;
+    }
+    if (message.includes('thank you' || 'thank you so much')) {
+        let finalresult = thank[Math.floor(Math.random() * thank.length)];
+        speech.text = finalresult;
+    }
+    if (message.includes('talk to you' || 'talk' || 'bye')) {
+        let finalresult = closing[Math.floor(Math.random() * closing.length)];
+        speech.text = finalresult;
+    }
+    window.speechSynthesis.speak(speech);
+    chatareamain.appendChild(showchatbotmsg(speech.text));
+}
+
+recognition.onresult = (e) => {
+    let resultIndex = e.resultIndex;
+    let transcript = e.results[resultIndex][0].transcript;
+    chatareamain.appendChild(showusermsg(transcript));
+    chatbotvoice(transcript);
+    console.log(transcript);
+}
+recognition.onend = () => {
+    mic.style.background = "rgb(34, 21, 55)";
+}
+mic.addEventListener("click", () => {
+    mic.style.background = '#39c81f';
+    recognition.start();
+    console.log("Activated");
+})
+
+
+let scene, camera, cloudParticles = [], composer, container = document.querySelector(".scene");
+
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.z = 1;
+    camera.rotation.x = 1.16;
+    camera.rotation.y = -0.12;
+    camera.rotation.z = 0.27;
+
+    let ambient = new THREE.AmbientLight(0x555555);
+    scene.add(ambient);
+
+    let directionalLight = new THREE.DirectionalLight(0xe419ff);
+    directionalLight.position.set(0, 0, 1);
+    scene.add(directionalLight);
+
+    let orangeLight = new THREE.PointLight(0xcc6600, 50, 450, 1.7);
+    orangeLight.position.set(200, 300, 100);
+    scene.add(orangeLight);
+    let redLight = new THREE.PointLight(0xd8547e, 50, 450, 1.7);
+    redLight.position.set(100, 300, 100);
+    scene.add(redLight);
+    let blueLight = new THREE.PointLight(0x5f26aa, 50, 450, 1.7);
+    blueLight.position.set(300, 300, 200);
+    scene.add(blueLight);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    scene.fog = new THREE.FogExp2(0x0d0116, 0.001);
+    renderer.setClearColor(scene.fog.color);
+    container.appendChild(renderer.domElement);
+
+    let loader = new THREE.TextureLoader();
+    loader.load("./images/smoke.png", function (texture) {
+        cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
+        cloudMaterial = new THREE.MeshLambertMaterial({
+            map: texture,
+            transparent: true
+        });
+
+        for (let p = 0; p < 50; p++) {
+            let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+            cloud.position.set(
+                Math.random() * 800 - 400,
+                500,
+                Math.random() * 500 - 500
+            );
+            cloud.rotation.x = 1.16;
+            cloud.rotation.y = -0.12;
+            cloud.rotation.z = Math.random() * 2 * Math.PI;
+            cloud.material.opacity = 0.55;
+            cloudParticles.push(cloud);
+            scene.add(cloud);
+        }
+    });
+    loader.load("./images/stars.jpg", function (texture) {
+
+        const textureEffect = new POSTPROCESSING.TextureEffect({
+            blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
+            texture: texture
+        });
+        textureEffect.blendMode.opacity.value = 0.2;
+
+        const bloomEffect = new POSTPROCESSING.BloomEffect({
+            blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
+            kernelSize: POSTPROCESSING.KernelSize.SMALL,
+            useLuminanceFilter: true,
+            luminanceThreshold: 0.3,
+            luminanceSmoothing: 0.75
+        });
+        bloomEffect.blendMode.opacity.value = 1.5;
+
+        let effectPass = new POSTPROCESSING.EffectPass(
+            camera,
+            bloomEffect,
+            textureEffect
+        );
+        effectPass.renderToScreen = true;
+
+        composer = new POSTPROCESSING.EffectComposer(renderer);
+        composer.addPass(new POSTPROCESSING.RenderPass(scene, camera));
+        composer.addPass(effectPass);
+
+        window.addEventListener("resize", onWindowResize, false);
+        render();
+    });
+}
+function onWindowResize() {
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(container.clientWidth, container.clientHeight);
+}
+function render() {
+    cloudParticles.forEach(p => {
+        p.rotation.z -= 0.001;
+    });
+    composer.render(0.1);
+    requestAnimationFrame(render);
+}
+init();
